946bb86fd99da078d571e89bd86abd897ac5ae7d
qjlxg <qjlxg@163.com>
2024-07-12 20:12:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ domain_blacklist = ["epg.pw"]
 url_keywords_blacklist = []
 open_subscribe = True
 subscribe_urls = [
-    "https://github.com/FunctionError/PiratesTv/raw/main/combined_playlist.m3u",
+    "https://github.com/qjlxg/TV/raw/main/list.txt",
     "https://github.com/qjlxg/collect-tv-txt/raw/main/merged_output.txt",
     "https://github.com/qjlxg/IPTVzb1/raw/main/%E7%BB%93%E6%9E%9C.txt",
     "https://github.com/qjlxg/IPTVzb1/raw/main/%E9%85%92%E5%BA%97%E6%BA%90.txt",
@@ -31,7 +31,6 @@ subscribe_urls = [
     "https://github.com/lalifeier/IPTV/raw/main/txt/hk.txt",
     "https://github.com/qjlxg/TVtest/raw/master/985.txt",
     "https://github.com/qjlxg/TVtest/raw/master/8460.txt",
-    "https://github.com/qjlxg/TVtest/raw/master/627.txt",
     "https://github.com/qjlxg/IPTVzb1/raw/main/%E6%B9%96%E5%8C%97%E7%94%B5%E4%BF%A1.txt",
     "https://github.com/qjlxg/IPTVzb1/raw/main/%E6%B9%96%E5%8C%97%E8%81%94%E9%80%9A.txt",
     "https://github.com/qjlxg/IPTVzb1/raw/main/%E6%B9%96%E5%8D%97%E7%94%B5%E4%BF%A1.txt",
