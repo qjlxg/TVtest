2b9217116f5218395b24f44a49b5ce74e5d83dbc
qjlxg <qjlxg@163.com>
2024-07-12 20:13:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,22 +18,7 @@ url_keywords_blacklist = []
 open_subscribe = True
 subscribe_urls = [
     "https://github.com/qjlxg/TV/raw/main/list.txt",
-    "https://github.com/qjlxg/collect-tv-txt/raw/main/merged_output.txt",
-    "https://github.com/qjlxg/IPTVzb1/raw/main/%E7%BB%93%E6%9E%9C.txt",
-    "https://github.com/qjlxg/IPTVzb1/raw/main/%E9%85%92%E5%BA%97%E6%BA%90.txt",
-    "https://github.com/qjlxg/IPTVzb1/raw/main/%E5%B9%BF%E4%B8%9C%E7%94%B5%E4%BF%A1.txt",
-    "https://github.com/qjlxg/iptv4/raw/master/iptv4.m3u",
-    "https://github.com/qjlxg/TV/raw/main/ru.txt",
-    "https://github.com/qjlxg/TV/raw/main/311.txt",
-    "https://github.com/fudong305/iptv9.github.io/raw/main/fu.txt",
-    "https://github.com/lalifeier/IPTV/raw/main/txt/IPTV.txt",
-    "https://github.com/lalifeier/IPTV/raw/main/txt/Local.txt",
-    "https://github.com/lalifeier/IPTV/raw/main/txt/hk.txt",
-    "https://github.com/qjlxg/TVtest/raw/master/985.txt",
-    "https://github.com/qjlxg/TVtest/raw/master/8460.txt",
-    "https://github.com/qjlxg/IPTVzb1/raw/main/%E6%B9%96%E5%8C%97%E7%94%B5%E4%BF%A1.txt",
-    "https://github.com/qjlxg/IPTVzb1/raw/main/%E6%B9%96%E5%8C%97%E8%81%94%E9%80%9A.txt",
-    "https://github.com/qjlxg/IPTVzb1/raw/main/%E6%B9%96%E5%8D%97%E7%94%B5%E4%BF%A1.txt",
+    
 ]
 open_multicast = True
 region_list = ["all"]
